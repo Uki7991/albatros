834132b8bb2c4143753c075091ff8952d589d06f
uki7991 <tilek.kubanov@gmail.com>
2017-03-25 19:34:20 +0600

    Add information in db_table and did public main

--- database/migrations/2017_03_25_131725_create_bookings_table.php
+++ database/migrations/2017_03_25_131725_create_bookings_table.php
@@ -13,7 +13,21 @@ class CreateBookingsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->integer('coming_day');
+            $table->integer('coming_month');
+            $table->integer('coming_year');
+            $table->integer('leaving_day');
+            $table->integer('leaving_month');
+            $table->integer('leaving_year');
+            $table->integer('hotel_number');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +37,6 @@ class CreateBookingsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('bookings');
     }
 }
